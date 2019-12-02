092e07e4f4748704ebd040393cab9b722cc11602
Bence Dányi <Bence.Danyi@ibm.com>
2019-12-02 10:40:47 +0100

    feat: add day 2 first

--- src/aoc/core.cljs
+++ src/aoc/core.cljs
@@ -36,9 +36,35 @@
        (map ->fuel-all)
        (reduce #(+ %1 %2) 0)))
 
+(defn step-intcode [pos code]
+  (case (get code pos)
+    99 nil
+    1 (assoc code (get code (+ pos 3))
+                  (+ (get code (get code (+ pos 1)))
+                     (get code (get code (+ pos 2)))))
+    2 (assoc code (get code (+ pos 3))
+                  (* (get code (get code (+ pos 1)))
+                     (get code (get code (+ pos 2)))))))
+
+(defn run-intcode [code]
+  (loop [code (-> code (assoc 1 12) (assoc 2 2))
+         pos 0]
+    (let [next (step-intcode pos code)]
+      (if (nil? next)
+        code
+        (recur next (+ pos 4))))))
+
+(defn d02a [input]
+  (->> (-> input (.split ","))
+       (map ->int)
+       (into [])
+       run-intcode
+       first))
+
 (defn -main [& args]
   (let [days {:d01a d01a
-              :d01b d01b}
+              :d01b d01b
+              :d02a d02a}
         day (keyword (str (first args) (second args)))
         input (read-input (first args))]
     (println ((day days) input))))
