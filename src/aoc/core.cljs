7f6225281716f3e0edf983b6291e6257192ec012
Bence Dányi <Bence.Danyi@ibm.com>
2019-12-02 12:19:02 +0100

    feat: add day 2 second

--- src/aoc/core.cljs
+++ src/aoc/core.cljs
@@ -46,8 +46,8 @@
                   (* (get code (get code (+ pos 1)))
                      (get code (get code (+ pos 2)))))))
 
-(defn run-intcode [code]
-  (loop [code (-> code (assoc 1 12) (assoc 2 2))
+(defn run-intcode [noun verb code]
+  (loop [code (-> code (assoc 1 noun) (assoc 2 verb))
          pos 0]
     (let [next (step-intcode pos code)]
       (if (nil? next)
@@ -58,13 +58,29 @@
   (->> (-> input (.split ","))
        (map ->int)
        (into [])
-       run-intcode
+       (run-intcode 12 2)
        first))
+
+(defn find-intcode [target code]
+  (->> (for [noun (range 0 100)
+             verb (range 0 100)] [noun verb])
+       (some (fn [[noun verb]]
+               (if (= target (-> (run-intcode noun verb code) first))
+                 [noun verb]
+                 nil)))))
+
+(defn d02b [input]
+  (->> (-> input (.split ","))
+       (map ->int)
+       (into [])
+       (find-intcode 19690720)
+       ((fn [[noun verb]] (+ (* noun 100) verb)))))
 
 (defn -main [& args]
   (let [days {:d01a d01a
               :d01b d01b
-              :d02a d02a}
+              :d02a d02a
+              :d02b d02b}
         day (keyword (str (first args) (second args)))
         input (read-input (first args))]
     (println ((day days) input))))
